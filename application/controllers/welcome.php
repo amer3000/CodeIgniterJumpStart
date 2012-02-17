65020a229b92c6fbed7603adf2cb6625a622514f
Amer Khalid <amer@amerkhalid.com>
2012-02-16 19:06:27 -0600

    Added TankAuth 1.0.9 for user management: https://github.com/ilkon/Tank-Auth

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,6 +1,15 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helper('url');
+		$this->load->library('tank_auth');
+	}
+
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +29,13 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+		if (!$this->tank_auth->is_logged_in()) {
+			redirect('/auth/login/');
+		} else {
+			$data['user_id']	= $this->tank_auth->get_user_id();
+			$data['username']	= $this->tank_auth->get_username();
+			$this->load->view('welcome', $data);
+		}
 	}
 }
 
