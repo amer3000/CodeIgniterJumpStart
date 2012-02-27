160a5b9b17eefc188947004bb38d45413fd73515
Amer Khalid <amer@amerkhalid.com>
2012-02-27 17:47:54 -0600

    Added HTML5 CSS3 Framework; Gridless: http://thatcoolguy.github.com/gridless-boilerplate/

--- application/views/layout.php
+++ application/views/layout.php
@@ -4,6 +4,27 @@
 		<meta charset="utf-8">
 		<title>Welcome to CodeIgniter Jump Start</title>
 
+		<!-- JS -->
+		<script type="text/javascript" src="<?=base_url()?>assets/js/jquery-1.7.1.min.js"></script>
+
+		<!-- Don't forget to update the bookmark icons in the root: http://mathiasbynens.be/notes/touch-icons -->
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="<?=base_url()?>assets/css/main.css?version=1" />
+
+		<!-- JavaScript -->
+		<!--[if IE]><![endif]-->
+		<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<script src="<?=base_url()?>assets/js/respond.min.js"></script>
+		<style>
+			#boxsize button, #boxsize input, #boxsize select, #boxsize textarea {
+				width: 200px;
+				border: 1px solid #333;
+			}
+		</style>
+
 	</head>
 	<body>
 
