436c1b1636fb7247d3c4e52dca2338e28d2dce06
Amer Khalid <amer@amerkhalid.com>
2012-02-27 16:11:23 -0600

    Setup layout file

---
+++ application/views/layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Welcome to CodeIgniter Jump Start</title>
+
+	</head>
+	<body>
+
+		<header>
+			<h1>Welcome to CodeIgniter Jump Start!</h1>
+		</header>
+		<section>
+			<?=$registerForm?>
+		</section>
+		<section>
+			<?=$loginForm?>
+		</section>
+
+		<footer>Uncopyright.</footer>
+	</body>
+</html>
